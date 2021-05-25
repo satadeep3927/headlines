92ae65a7e5677f66966850d15ac2f703da30a86e
satadeep3927 <63926708+satadeep3927@users.noreply.github.com>
2021-05-26 01:01:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -232,7 +232,7 @@ var loadwallpapers = (e)=>{
                 for(let i = 0; i<15;i++){
                 document.getElementById("all-wallpaper-container").innerHTML+=`<div class="wallpapers" style="background-image:url('./weather-images/09n.gif')">
                 <div class="details"><br><h4 class="quotes">${array[i]}</h4><br><p class="meta-text" style="margin-bottom:14px">&#169; Unplash images</p>
-                <a class="d-btn" onclick="downloadimage(event)">Download</a>
+                ${downloadimage(data.result[i].image)}
                 </div>
                 </div>`
                 }
@@ -251,17 +251,14 @@ var loadwallpapers = (e)=>{
     document.getElementById('all-wallpaper-container').style.display="block"
 
 }
-    async function downloadimage(imageSrc,e) {
-        const image = await fetch('https://images.unsplash.com/photo-1565176083969-d75f4239ba4d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzI4MzV8MHwxfHNlYXJjaHw3MjJ8fG1vYmlsZSUyMHdhbGxwYXBlcnxlbnwwfHx8fDE2MjE4Nzk4NDE&ixlib=rb-1.2.1&q=80&w=1080')
+    async function downloadimage(imageSrc) {
+        const image = await fetch(imageSrc)
         const imageBlog = await image.blob()
         const imageURL = URL.createObjectURL(imageBlog)
       
         const link = document.createElement('a')
         link.href = imageURL
         link.download = 'wallpaper.jpg'
-        document.body.appendChild(link)
-        link.click()
-        document.body.removeChild(link)
-        e.preventDefault();
+        return link;
 
 }
